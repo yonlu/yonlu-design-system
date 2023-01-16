d7ca6a81c4338fc12767a5c4f501fdf7e57d8321
yonlu <lss.emailbox@gmail.com>
2023-01-16 15:50:41 -0500

    feat: add Button component

---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,92 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '@stitches/react'
+
+export const Button = styled('button', {
+  all: 'unset',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 120,
+  boxSizing: 'border-box',
+  padding: '0 $4',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  '&:disabled': {
+    backgroundColor: '$gray200',
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$yonlu500',
+
+        '&:not(:disabled):hover': {
+          background: '$yonlu300',
+        },
+      },
+
+      secondary: {
+        color: '$yonlu300',
+        border: '2px solid $yonlu500',
+
+        '&:not(:disabled):hover': {
+          background: '$yonlu500',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          background: '$gray200',
+          color: '$gray200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        padding: '0 $4',
+        height: 38,
+      },
+
+      md: {
+        padding: '0 $4',
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
